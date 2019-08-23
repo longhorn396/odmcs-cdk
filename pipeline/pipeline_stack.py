0aedf1b999c1b7eb803bf14830ecf1ede170d51e
Devin Drawhorn <devin.drawhorn@gartner.com>
2019-08-23 13:17:45 -0500

    Not updating from apt-get

--- pipeline/pipeline_stack.py
+++ pipeline/pipeline_stack.py
@@ -51,7 +51,6 @@ class PipelineStack(core.Stack):
                             "python": 3.7
                         },
                         "commands": [
-                            "apt-get update -y",
                             "npm install -g aws-cdk",
                             "pip install --upgrade pip",
                             "pip install -r requirements.txt",
